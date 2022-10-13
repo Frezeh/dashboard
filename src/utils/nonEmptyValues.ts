2adda5849b7ab6be44fa1ef18bde9a7f8a32afc0
Frezeh <ezehfrank87@gmail.com>
2022-10-13 14:14:49 +0100

    update

--- src/utils/nonEmptyValues.ts
+++
@@ -1,9 +0,0 @@
-import { SortProps } from "../types";
-
-function nonEmptyValues (values: SortProps) {
-        for (const [key, value] of Object.entries(values)) {
-            if(value === "") {
-                
-            }
-        }      
-}
